f7408b5281604abc64cd8c40aa0033ea35c28ae2
HanaAbdelbari <hanaabdelbari882@gmail.com>
2026-06-04 10:16:08 +0300

    Initial commit

---
+++ src/main/java/com/marketplace/todomeetusvr/ToDoMeetUsVrApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.todomeetusvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoMeetUsVrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoMeetUsVrApplication.class, args);
+    }
+
+}
